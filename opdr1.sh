a6aea53bdc29b5d7df6b4c8b63644baf193434eb
Dennis Dollee <dennisdollee@gmail.com>
2018-09-07 15:29:17 +0200

    QC script now takes files as arguments

--- opdr1.sh
+++ opdr1.sh
@@ -5,31 +5,33 @@ declare -i min=0
 declare -i max=0
 declare -i tot=0
 
-while read p; do
-    #count lines
-    count=$((${count}+1))
-    #only shows read
-    if [[ $((${count}%4)) -eq 2 ]] ; then
-        ## ESCAPE BACKSLASH
-        echo $p
-        length=$(echo ${p} | wc -c)
-        tot+=${length}
-        if [[ ${min} -eq 0 ]] ; then
-            min=$length
-            max=$length
-        else
-            if [[ $min -gt $length ]] ; then
+for file in "$@"; do
+    while read p; do
+        #count lines
+        count=$((${count}+1))
+        #only shows read
+        if [[ $((${count}%4)) -eq 2 ]] ; then
+            ## ESCAPE BACKSLASH
+            echo $p
+            length=$(echo ${p} | wc -c)
+            tot+=${length}
+            if [[ ${min} -eq 0 ]] ; then
                 min=$length
-            fi
-            if [[ $max -lt $length ]] ; then
                 max=$length
+            else
+                if [[ $min -gt $length ]] ; then
+                    min=$length
+                fi
+                if [[ $max -lt $length ]] ; then
+                    max=$length
+                fi
             fi
+            ## CALC GC
+            ## CALC GC PER POSITION
         fi
-        ## CALC GC
-        ## CALC GC PER POSITION
-    fi
-done < testbestand2.fastq
-
+    done < $file
 echo "min: " $min
 echo "max: " $max
 echo "Avg: " $((${tot}/(${count}/4)))
+echo $file
+done
