2feb1e2ca7ca52dc48eafd428517cd20196aceb6
Dennis Dollee <dennisdollee@gmail.com>
2018-09-27 00:56:28 +0200

    trim cpp updated

--- trim.cpp
+++ trim.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
@@ -8,26 +9,52 @@ int main(int argc, char **argv) {
     for(int i = 1; i < argc; i++)
     {
         int count = 0;
+        string id;
+        string seq;
+        vector<int> qscore;
 
         ifstream ifile(argv[i]);
         string line;
 
         // loops through file lines
         while(ifile >> line) {
-            if (count==4) {
-                count = 0;
-            }
-                cout << line[0] << count << endl;
+            // cout << count << line << endl;
+            
+            cout << count << " : "; 
+            switch (count)
+            {
+                case 0:
+                    cout << line << endl;
+                    break;
+            
+                case 1:
+                    cout << line << " ASD " << endl;
+                    break;
 
-            // cout << line << endl;
-            count++;
+                case 2:
+                    cout << "+++++++++++++++++++" << endl;
+                    break;
+
+                case 3:
+                    cout << "ASCIIIIIIIIIIIIIIIIIIIIII" << endl;
+                    break;
+
+                default:
+                    break;
+            }
+
+            count < 3 ? count++ : count=0;  
         }
         cout << argv[i] << endl;
 
-        int num = '!';
-        cout << num;
+        // int num = '!';
+        // cout << num;
     }
 
     
     
+}
+
+int getQscore(string seq) {
+
 }
